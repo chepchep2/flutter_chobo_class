3d9f39b232d3a9e0908f9dec9c2f0077ce6f32d4
sangwoo <kajer102@gmail.com>
2024-01-02 00:07:23 +0900

    feat: streambuilder 사용한 방식, 카운터앱에 뷰모델 사용

--- lib/23_12_29/00_view_model_counter_app.dart
+++ lib/23_12_29/00_view_model_counter_app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chobo_class/23_12_29/counter_view_model.dart';
 
@@ -11,12 +10,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -28,15 +36,20 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
               '${viewModel.counter}',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
@@ -47,7 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
             viewModel.incrementCounter();
           });
         },
-      ),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
