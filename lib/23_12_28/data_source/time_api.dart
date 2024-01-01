9e01bb77e599f09bc699c0628aa424c0ceee5e96
sangwoo <kajer102@gmail.com>
2024-01-01 23:53:04 +0900

    feat: future를 사용한 원래 방식

--- lib/23_12_28/data_source/time_api.dart
+++ lib/23_12_28/data_source/time_api.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
+import 'package:flutter_chobo_class/23_12_28/model/time.dart';
 import 'package:http/http.dart' as http;
 
-// class TimeApi {
-//   Future<Time> getTime() async {
-//     final response = await http.get(Uri.parse('uri'));
-//     return TIme.fromJson(jsonDecode(response.body));
-//   }
-// }
+class TimeApi {
+  Future<Time> getTime() async {
+    final response = await http.get(Uri.parse('uri'));
+    return Time.fromJson(jsonDecode(response.body));
+  }
+}
 
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,21 +15,55 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SeoulTimeApp(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class SeoulTimeApp extends StatefulWidget {
-  const SeoulTimeApp({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  State<SeoulTimeApp> createState() => _SeoulTimeAppState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _SeoulTimeAppState extends State<SeoulTimeApp> {
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
