64e7e7e7559a3cac273c0f2ba6f3899f0c34c6f2
sangwoo <kajer102@gmail.com>
2024-01-01 23:58:10 +0900

    feat: futurebuilder를 사용한 방식

---
+++ lib/23_12_28/01_futurebuilder_seoul_time_app.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chobo_class/23_12_28/data_source/time_api.dart';
+import 'package:flutter_chobo_class/23_12_28/model/time.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SeoulTimeApp(),
+    );
+  }
+}
+
+class SeoulTimeApp extends StatelessWidget {
+  const SeoulTimeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FutureBuilder 사용 방식'),
+      ),
+      body: FutureBuilder(
+        future: TimeApi().getTime(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text('데이터가 없습니다.'),
+            );
+          }
+
+          final Time time = snapshot.data!;
+
+          return Center(
+            child: Column(
+              children: [
+                Text(
+                  time.time,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                Text(
+                  time.utcTime,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
